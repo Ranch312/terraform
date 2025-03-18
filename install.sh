03a4cc678e90265377391436442d0a533cd99add
Ranch312 <joseph.collins@bsu.edu>
2025-03-18 13:12:55 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+mkdir terraform-conf
+cd terraform-conf
+curl -LO https://raw.github.com/robertpeteuil/terraform-installer/master/terraform->
+apt-get install unzip
+chmod +x terraform-install.sh
+./terraform-install.sh
+curl -LO https://raw.githubusercontent.com/Ranch312/terraform/main/main.tf
+terraform init
