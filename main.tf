e6d2f0cc13716dabd15db94ae331211e3bf535c8
Ranch312 <joseph.collins@bsu.edu>
2025-03-21 10:13:55 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 
 provider "proxmox" {
 # pm_api_url will change depending on which Dataceneter you are in ie: ProdCluster
-    pm_api_url          = "https://172.27.150.***:8006/api2/json"
+    pm_api_url          = "https://***:8006/api2/json"
     pm_api_token_id     = "your-token-id"
     pm_api_token_secret = "your-secret"
     pm_tls_insecure     = true
